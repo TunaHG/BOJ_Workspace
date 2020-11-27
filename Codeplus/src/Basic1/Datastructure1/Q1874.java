3c52f1c0f7ebcad5b3dc43e915a4e6f1fcb963b8
TunaHG <asdf0185@naver.com>
2020-11-27 19:04:56 +0900

    Solved: BOJ Question 1874 solve

---
+++ Codeplus/src/Basic1/Datastructure1/Q1874.java
@@ -0,0 +1,42 @@
+package Basic1.Datastructure1;
+
+import java.util.Stack;
+import java.util.Scanner;
+
+public class Q1874 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int N = scan.nextInt();
+		Stack<Integer> stack = new Stack<Integer>();
+		StringBuilder sb = new StringBuilder();
+		
+		int idx = 1;
+		for(int i = 0; i < N; i++) {
+			int num = scan.nextInt();
+			while(true) {
+				if(stack.empty()) {
+					stack.push(idx++);
+					sb.append("+\n");
+				}
+				if(stack.peek().equals(num)) {
+					stack.pop();
+					sb.append("-\n");
+					break;
+				}
+				else if(stack.peek() > num) {
+					System.out.println("NO");
+					scan.close();
+					return;
+				}
+				stack.push(idx++);
+				sb.append("+\n");
+			}
+		}
+		sb.deleteCharAt(sb.length() - 1);
+		System.out.println(sb.toString());
+		scan.close();
+	}
+
+}
