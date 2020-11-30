53a17523b9a6a63211aed5b306dbeb67fdcf86af
TunaHG <asdf0185@naver.com>
2020-11-30 11:30:55 +0900

    Solved: BOJ Question 1158 solve

---
+++ Codeplus/src/Basic1/Datastructure1/Q1158.java
@@ -0,0 +1,37 @@
+package Basic1.Datastructure1;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Q1158 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Queue<Integer> queue = new LinkedList<Integer>();
+		
+		int N = scan.nextInt();
+		int K = scan.nextInt();
+		
+		for(int i = 1; i <= N; i++) {
+			queue.add(i);
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		int idx = 1;
+		sb.append("<");
+		while(!queue.isEmpty()) {
+			if(idx % K == 0) {
+				sb.append(queue.poll() + ", ");
+			}
+			else {
+				queue.add(queue.poll());
+			}
+			idx++;
+		}
+		sb.replace(sb.length() - 2, sb.length() - 1, ">");
+		System.out.println(sb.toString());
+		scan.close();
+	}
+
+}
