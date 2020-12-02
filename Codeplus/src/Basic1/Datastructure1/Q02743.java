974facd44799d5b1f087546b1f449a6fce054e2a
TunaHG <asdf0185@naver.com>
2020-12-02 12:10:24 +0900

    Solved: BOJ Question 2743 solve

---
+++ Codeplus/src/Basic1/Datastructure1/Q02743.java
@@ -0,0 +1,17 @@
+package Basic1.Datastructure1;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q02743 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String input = br.readLine();
+		
+		System.out.println(input.length());
+	}
+
+}
