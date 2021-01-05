c1f00bfcbd10215b1f14e3e141d3357c0fde0260
TunaHG <asdf0185@naver.com>
2021-01-05 10:44:48 +0900

    Solved: BOJ Question 15990 solve, be careful with the Integer value limit

---
+++ Codeplus/src/Basic1/DP1/Q15990.java
@@ -0,0 +1,34 @@
+package Basic1.DP1;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q15990 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		int dp[][] = new int[100001][4];
+		dp[0][1] = 1;
+		dp[1][1] = 1;
+		dp[2][2] = 1;
+		
+		for(int i = 3; i < 100001; i++) {
+			dp[i][1] = (dp[i - 1][2] + dp[i - 1][3]) % 1000000009;
+			dp[i][2] = (dp[i - 2][1] + dp[i - 2][3]) % 1000000009;
+			dp[i][3] = (dp[i - 3][1] + dp[i - 3][2]) % 1000000009;
+		}
+		
+		while(T-- > 0) {
+			int n = Integer.parseInt(br.readLine());
+			
+			System.out.println(((dp[n][1] + dp[n][2]) % 1000000009 + dp[n][3]) % 1000000009);
+		}
+	}
+
+}
+
+// 99991 반례
