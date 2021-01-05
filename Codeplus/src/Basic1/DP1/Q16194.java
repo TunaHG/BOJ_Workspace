2c68f7c7bbc6138faf5f64772e1d4f7fb111745e
TunaHG <asdf0185@naver.com>
2021-01-05 10:09:00 +0900

    Solved: BOJ Question 16194 solve

---
+++ Codeplus/src/Basic1/DP1/Q16194.java
@@ -0,0 +1,29 @@
+package Basic1.DP1;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q16194 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		String input[] = br.readLine().split(" ");
+		
+		int dp[] = new int[N];
+		for(int i = 0; i < N; i++) {
+			dp[i] = Integer.parseInt(input[i]);
+		}
+		
+		for(int i = 1; i < N; i++) {
+			for(int j = 0; j < i; j++) {
+				dp[i] = Math.min(dp[i], dp[j] + Integer.parseInt(input[i - j - 1]));
+			}
+		}
+		
+		System.out.println(dp[N - 1]);
+	}
+
+}
