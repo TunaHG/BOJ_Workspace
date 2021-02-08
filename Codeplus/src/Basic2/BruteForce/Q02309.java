10dec581651f5d70dee185e98336a590362147f4
TunaHG <asdf0185@naver.com>
2021-02-08 21:17:21 +0900

    Solved: BOJ Question 2309 solve

---
+++ Codeplus/src/Basic2/BruteForce/Q02309.java
@@ -0,0 +1,35 @@
+package Basic2.BruteForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Q02309 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[9];
+        int sum = 0;
+
+        for(int i = 0; i < 9; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+
+        Arrays.sort(arr);
+
+        A: for(int i = 0; i < 8; i++){
+            for(int j = i; j < 9; j++){
+                if(sum - arr[i] - arr[j] == 100) {
+                    for(int k = 0; k < 9; k++){
+                        if(k == i || k == j)
+                            continue;
+                        System.out.println(arr[k]);
+                    }
+                    break A;
+                }
+            }
+        }
+    }
+}
