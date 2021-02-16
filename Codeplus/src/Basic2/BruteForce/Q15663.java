4e4c2a09bc587325517c3cef1d851ad22fd4b02f
TunaHG <asdf0185@naver.com>
2021-02-16 21:18:06 +0900

    Solved: BOJ Question 15663 solve, using LinkedHashSet

---
+++ Codeplus/src/Basic2/BruteForce/Q15663.java
@@ -0,0 +1,58 @@
+package Basic2.BruteForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
+public class Q15663 {
+    static int N, M;
+    static int[] arr;
+    static int[] answer;
+    static boolean[] visited;
+    static LinkedHashSet<String> set;
+    public static void NM(int count) {
+        if(count == M) {
+            StringBuilder sb = new StringBuilder();
+            for(int i : answer) {
+                sb.append(i);
+                sb.append(" ");
+            }
+            set.add(sb.toString().trim());
+            return ;
+        }
+
+        for(int i = 0; i < N; i++) {
+            if(visited[i])
+                continue;
+            visited[i] = true;
+            answer[count] = arr[i];
+            NM(count + 1);
+            visited[i] = false;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+
+        arr = new int[N];
+        visited = new boolean[N];
+        answer = new int[M];
+        set = new LinkedHashSet<>();
+
+        input = br.readLine().split(" ");
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+        Arrays.sort(arr);
+
+        NM(0);
+        for(String str : set) {
+            System.out.println(str);
+        }
+    }
+}
